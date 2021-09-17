55d8c0bf1a604fe7121ceccac7d402be058c2496
tsarapoid <nadein@gmail.com>
2021-09-17 22:04:40 +0300

    alphabetSubSequence

--- ALGO/alphabetSubSequence/alphabetSubSequence.ts
+++ ALGO/alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,18 @@
 export function alphabetSubsequence(s: string): boolean {
+  let count = 0
 
+  const sequence = s.split("").sort()
+  console.log(sequence)
+
+  sequence.reduce((prev, curr) => {
+    if (prev === curr) ++count
+    return curr
+  })
+  return count < 1
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence("zab"))
+console.log(alphabetSubsequence("effg"))
+console.log(alphabetSubsequence("cdce"))
+console.log(alphabetSubsequence("ace"))
+console.log(alphabetSubsequence("bxz"))
